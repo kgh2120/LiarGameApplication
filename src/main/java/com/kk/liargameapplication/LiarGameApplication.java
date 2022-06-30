116b91549de2cd949592a1d3d91623fb216f1baa
kyoohyun <kgh2120@naver.com>
2022-06-30 18:18:27 +0900

    Initial commit

---
+++ src/main/java/com/kk/liargameapplication/LiarGameApplication.java
@@ -0,0 +1,13 @@
+package com.kk.liargameapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiarGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiarGameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kk/liargameapplication/LiarGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kk.liargameapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiarGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
